c17468a181818dbe72b19bc32c097153d6d1ec70
harshavardhankalyanikar <harshavardhankalyanikar@gmail.com>
2026-06-17 10:29:53 +0530

    Update auth schema

--- backend/app/auth_schema.py
+++ backend/app/auth_schema.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr, Field
 
 class RegisterSchema(BaseModel):
     name: str
-    email: str
-    password: str
+    email: EmailStr
+    password: str = Field(min_length=6)
 
 
 class LoginSchema(BaseModel):
